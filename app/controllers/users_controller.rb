bdd5be556d52cc3a1ef345fd3b64a3a68f319c47
Kurt Edelbrock <kurtedelbrock@gmail.com>
2013-11-12 15:08:35 -0500

    User update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	
-	protect_from_forgery :except => :create
+protect_from_forgery :except => [:create, :update]
 	
 	def index
 		@users = User.list.all
@@ -13,6 +13,15 @@ class UsersController < ApplicationController
   def create
 	  @user = User.new(params)
 	  @user.save
+	end
+	
+	def update
+		@user = User.get(params[:id])
+		@user.update_attributes(params)
+  end
+	
+	def destroy
+		
 	end
 	
 end
