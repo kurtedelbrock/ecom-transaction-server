c4284bbb45540c6ac5706479e1cc6eab94d15be3
Kurt Edelbrock <kurtedelbrock@gmail.com>
2013-11-12 14:57:49 -0500

    User add created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
 	
+	protect_from_forgery :except => :create
+	
 	def index
 		@users = User.list.all
 	end
 	
 	def show
-		@user = User.get(params[:id])
+	  @user = User.get(params[:id])
+  end
+  
+  def create
+	  @user = User.new(params)
+	  @user.save
 	end
 	
 end
---
+++ app/views/users/create.json.jbuilder
@@ -0,0 +1,1 @@
+json.(@user, :id, :first_name, :last_name, :birth_date, :wine_api_id, :quiz_answers, :shipping_address, :billing_address, :transactions, :updated_at, :created_at)
