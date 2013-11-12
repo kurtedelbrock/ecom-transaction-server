cd93efe6c3aad8abe675a3a2489bf308aab63209
Kurt Edelbrock <kurtedelbrock@gmail.com>
2013-11-12 11:37:26 -0500

    Added main application route

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < CouchRest::Model::Base
 
 	property :first_name,				String
 	property :last_name,					String
+	
+	property :birth_date,				Date
+	
 	property :wine_api_id,				Integer
 	
 	property :quiz_answers,			QuizAnswer, :array => true
