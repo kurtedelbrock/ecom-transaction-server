f2f2dca5dd0668916635ee1ed6050d488f4f851a
Kurt Edelbrock <kurtedelbrock@gmail.com>
2013-11-11 20:41:49 -0500

    Switching from couchbase to couchrest

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.0.1'
 gem 'jbuilder', '~> 1.2'
-gem 'couchbase-model'
+gem 'couchrest_model'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,6 @@ require "action_mailer/railtie"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-require "couchbase/model"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
