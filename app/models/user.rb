79b213d5c8e5014bc19a90259a5fe3c741d97649
Kurt Edelbrock <kurtedelbrock@gmail.com>
2013-11-11 21:02:01 -0500

    Added default object type to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < CouchRest::Model::Base
 	property :last_name,					String
 	property :wine_api_id,				Integer
 	
+	property :object_type,							String, :default => "user", :read_only => true
+	
 	timestamps!
 	
 end
