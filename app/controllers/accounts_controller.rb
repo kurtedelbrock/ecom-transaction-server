153e096b43d9b407a41cb20ff2319397959dceff
Kurt Edelbrock <kurtedelbrock@gmail.com>
2013-11-13 04:27:43 -0500

    Adding authorization

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -35,7 +35,7 @@ class AccountsController < ApplicationController
   def options
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Content-Type'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Content-Type, Authorization'
     headers['Access-Control-Max-Age'] = '1728000'
     render :text => '', :content_type => 'text/plain'
   end
